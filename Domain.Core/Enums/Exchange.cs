a41e0e92b080d79ec881266f6a9497860e0bbd10
AndisPapageorgiou <42977189+andispapageo@users.noreply.github.com>
2026-01-06 09:15:00 +0200

    ep-running code cleanup profile1 (#2)

--- Domain.Core/Enums/Exchange.cs
+++ Domain.Core/Enums/Exchange.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Core.Enums
+﻿namespace Domain.Core.Enums
 {
     public enum Exchange
     {
