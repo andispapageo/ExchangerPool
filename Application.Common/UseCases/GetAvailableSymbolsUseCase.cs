e3fbf3826742c0076af6611a843a31cfb58f0f4b
AndisPapageorgiou <42977189+andispapageo@users.noreply.github.com>
2026-01-10 14:32:48 +0200

    Develop (#5)

--- Application.Common/UseCases/GetAvailableSymbolsUseCase.cs
+++ Application.Common/UseCases/GetAvailableSymbolsUseCase.cs
@@ -1,18 +1,9 @@
 ﻿using Application.Common.DTOs;
 using Application.Common.Mappings;
 using Domain.Core.Interfaces;
-
-namespace Application.Common.UseCases
+namespace Application.Common.UseCases;
+public sealed record GetAvailableSymbolsUseCase(ILiquidityAggregator aggregator)
 {
-    public sealed class GetAvailableSymbolsUseCase
-    {
-        private readonly ILiquidityAggregator _aggregator;
-        public GetAvailableSymbolsUseCase(ILiquidityAggregator aggregator) => _aggregator = aggregator;
-
-        public async Task<IEnumerable<CryptoSymbolDto>> ExecuteAsync(CancellationToken cancellationToken = default)
-        {
-            var results = await _aggregator.GetAvailableSymbolsAsync(cancellationToken);
-            return results.Select(r => r.ToDto());
-        }
-    }
+    public async Task<IEnumerable<CryptoSymbolDto>> ExecuteAsync(CancellationToken cancellationToken = default)
+        => (await aggregator.GetAvailableSymbolsAsync(cancellationToken)).Select(r => r.ToDto());
 }
