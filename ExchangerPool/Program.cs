90b125c585b2536b43b87adf1aab44efc65bb213
AndisPapageorgiou <andispapageo4@outlook.com>
2026-01-10 14:25:38 +0200

    ep-removal of whitespaces

--- ExchangerPool/Program.cs
+++ ExchangerPool/Program.cs
@@ -1,14 +1,10 @@
 using ExchangerPool.Configs;
 var builder = WebApplication.CreateBuilder(args);
-
 builder.AddServiceDefaults();
 using var loggerFactory = LoggerFactory.Create(config => config.AddConsole());
 var startupLogger = loggerFactory.CreateLogger<Program>();
-
 builder.AddServiceConfigs(startupLogger);
 
-
 var app = builder.Build();
-
 await app.UseAppMiddleware(CancellationToken.None);
 await app.RunAsync();
